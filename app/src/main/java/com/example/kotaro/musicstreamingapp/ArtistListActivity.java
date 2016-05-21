e0fbbcd004279afea9998936231cf99c244aab39
kotaro <skouta310@gmail.com>
2016-05-21 17:16:58 +0900

    [Add] ArtistListActivity & MusicListActivity

---
+++ app/src/main/java/com/example/kotaro/musicstreamingapp/ArtistListActivity.java
@@ -0,0 +1,10 @@
+package com.example.kotaro.musicstreamingapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kotaro on 16/05/21.
+ */
+public class ArtistListActivity extends AppCompatActivity{
+
+}
---
+++ app/src/main/java/com/example/kotaro/musicstreamingapp/MusicListActivity.java
@@ -0,0 +1,16 @@
+package com.example.kotaro.musicstreamingapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kotaro on 16/05/21.
+ */
+public class MusicListActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
